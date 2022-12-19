165468dc61b168e7abb724463d3e45d543eb888f
Suleyman Hanyyev <solomonNSI@users.noreply.github.com>
2022-12-19 22:46:29 +0300

    fix/ api to the model

--- aura-palette-model/server.py
+++ aura-palette-model/server.py
@@ -1,56 +1,5 @@
-from flask import Flask, jsonify, request
-from flask_cors import CORS
-from gevent.pywsgi import WSGIServer
-import argparse
-import multiprocessing as mp
-import os
-
-from solver import Solver
-
-
-# Declare a flask app
-app = Flask(__name__)
-
-
-# Enable CORS
-cors = CORS(app)
-
-# Global variables
-
-# Constants
-PORT = 8000
-
-
-# Placeholder for Solver object
-solver = None
-
-
-def log(f, s, level='DEBUG'):
-    f.write('[{}]: {}\n'.format(level, s))
-
-
-# Healthcheck endpoint
-@app.route('/',  methods=['GET'])
-def healthCheck():
-    return jsonify(status='OK', message='Server is running')
-
-
-# Palette sample endpoint
-@app.route('/palette',  methods=['GET','POST'])
-def samplePalette():
-
-    if not request.data:
-        return jsonify(status='Error', message='Empty body')
-
-    queryStrings = request.json['queryStrings']
-    numPalettesPerQuery = request.json['numPalettesPerQuery']
-    
-    palettes = solver.sample_TPN(queryStrings, numPalettesPerQuery)
-
-    return jsonify(status='OK', message={'palettes': palettes})
-
-
-if __name__ == '__main__':
+    import argparse
+    from solver import Solver
 
     parser = argparse.ArgumentParser()
 
@@ -68,7 +17,7 @@ if __name__ == '__main__':
     parser.add_argument('--dataset', type=str, default='bird256', choices=['imagenet', 'bird256'])
     parser.add_argument('--lr', type=float, default=5e-4, help='initial learning rate')
     parser.add_argument('--num_epochs', type=int, default=1000, help='number of epochs for training')
-    parser.add_argument('--resume_epoch', type=int, default=None, help='resume training from this epoch')
+    parser.add_argument('--resume_epoch', type=int, default=1000, help='resume training from this epoch')
     parser.add_argument('--batch_size', type=int, default=1, help='batch size for sampling')
     parser.add_argument('--dropout_p', type=float, default=0.2)
     parser.add_argument('--weight_decay', type=float, default=5e-5)
@@ -80,6 +29,7 @@ if __name__ == '__main__':
 
     # Directories.
     parser.add_argument('--text2pal_dir', type=str, default='./models/TPN')
+    parser.add_argument('--pal2color_dir', type=str, default='./models/PCN')
     parser.add_argument('--train_sample_dir', type=str, default='./samples/train')
     parser.add_argument('--test_sample_dir', type=str, default='./samples/test')
 
@@ -90,16 +40,11 @@ if __name__ == '__main__':
                         help='how many steps to wait before saving the training output')
     parser.add_argument('--save_interval', type=int, default=50,
                         help='how many steps to wait before saving the trained models')
-    args = parser.parse_args()
+    args, unknown = parser.parse_known_args()
     print(args)
     
     # Solver for sampling Text2Colors.
     solver = Solver(args)
 
-    mp.set_start_method('spawn')
-
-    # app.run(debug=False, host='0.0.0.0', port=PORT)
-
-    # Serve the app with gevent
-    http_server = WSGIServer(('0.0.0.0', PORT), app)
-    http_server.serve_forever()
+    query = ["your query here", "your query here"]
+    print(solver.sample_TPN(query))
