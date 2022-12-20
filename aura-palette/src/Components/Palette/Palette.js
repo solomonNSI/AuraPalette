1df6c45cd28e808c6f81bf294d8b2648ec60ff44
Can Avşar <can.avsar@ug.bilkent.edu.tr>
2022-12-20 12:13:01 +0300

    Color modes

--- aura-palette/src/Components/Palette/Palette.js
+++ aura-palette/src/Components/Palette/Palette.js
@@ -1,42 +1,88 @@
 import React, { useState } from "react";
 import { ExportIcon } from "../../Icons/ExportIcon";
 import * as S from "./style";
+import { hexToRgb, hslToHex } from "../../Helpers/ColorWizard";
 
 export const Palette = ({ palette }) => {
-  const [colorMode, setColorMode] = useState("RGB");
+  const [colorMode, setColorMode] = useState("HEX");
 
   const changeColorMode = () => {
-    if (colorMode === "RGB") setColorMode("HSB");
-    else setColorMode("RGB");
+    if (colorMode === "HEX") setColorMode("RGB");
+    else setColorMode("HEX");
+  };
+
+  function displayPaletteColors(paletteNumber) {
+    console.log(palette[paletteNumber]);
+    if (colorMode === "RGB") return hexToRgb(palette[paletteNumber]);
+    if (colorMode === "HEX") return palette[paletteNumber].toUpperCase();
   }
 
   return (
     <S.Container>
       <S.Header>
         <S.PaletteTitle>Palette</S.PaletteTitle>
-        <S.ColorModeButton onClick={changeColorMode}>{colorMode}</S.ColorModeButton>
+        <S.ColorModeButton onClick={changeColorMode}>
+          Color Mode: {colorMode}
+        </S.ColorModeButton>
         <S.StyledStarIcon height="20px" />
         <ExportIcon height="20px" />
       </S.Header>
 
       <S.Colors>
         <S.Color colorHex={palette[0]}>
-          <S.ColorCode onClick={() => {navigator.clipboard.writeText(palette[0]).toUpperCase()}}>{(palette[0]).toUpperCase()}</S.ColorCode>
+          <S.ColorCode
+            onClick={() => {
+              navigator.clipboard.writeText(displayPaletteColors(0));
+            }}
+          >
+            {displayPaletteColors(0)}
+          </S.ColorCode>
         </S.Color>
         <S.Color colorHex={palette[1]}>
-          <S.ColorCode onClick={() => {navigator.clipboard.writeText(palette[1]).toUpperCase()}}>{(palette[1]).toUpperCase()}</S.ColorCode>
+          <S.ColorCode
+            onClick={() => {
+              navigator.clipboard
+                .writeText(displayPaletteColors(1))
+                .toUpperCase();
+            }}
+          >
+            {displayPaletteColors(1)}
+          </S.ColorCode>
         </S.Color>
         <S.Color colorHex={palette[2]}>
-          <S.ColorCode onClick={() => {navigator.clipboard.writeText(palette[2]).toUpperCase()}}>{(palette[2]).toUpperCase()}</S.ColorCode>
+          <S.ColorCode
+            onClick={() => {
+              navigator.clipboard
+                .writeText(displayPaletteColors(2))
+                .toUpperCase();
+            }}
+          >
+            {displayPaletteColors(2)}
+          </S.ColorCode>
         </S.Color>
         <S.Color colorHex={palette[3]}>
-          <S.ColorCode onClick={() => {navigator.clipboard.writeText(palette[3]).toUpperCase()}}>{(palette[3]).toUpperCase()}</S.ColorCode>
+          <S.ColorCode
+            onClick={() => {
+              navigator.clipboard
+                .writeText(displayPaletteColors(3))
+                .toUpperCase();
+            }}
+          >
+            {displayPaletteColors(3)}
+          </S.ColorCode>
         </S.Color>
         <S.Color colorHex={palette[4]}>
-          <S.ColorCode onClick={() => {navigator.clipboard.writeText(palette[4]).toUpperCase()}}>{(palette[4]).toUpperCase()}</S.ColorCode>
+          <S.ColorCode
+            onClick={() => {
+              navigator.clipboard
+                .writeText(displayPaletteColors(4))
+                .toUpperCase();
+            }}
+          >
+            {displayPaletteColors(4)}
+          </S.ColorCode>
         </S.Color>
       </S.Colors>
-      
     </S.Container>
   );
 };
