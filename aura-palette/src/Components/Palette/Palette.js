ca7cfd30254c133fa73d85ea38cd4e28288548c2
Can Avşar <can.avsar@ug.bilkent.edu.tr>
2022-12-20 13:13:54 +0300

    CopyIcon active

--- aura-palette/src/Components/Palette/Palette.js
+++ aura-palette/src/Components/Palette/Palette.js
@@ -45,7 +45,16 @@ export const Palette = ({ palette }) => {
                 navigator.clipboard.writeText(displayPaletteColors(0));
               }}
             >
-              <CopyIcon />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="18px"
+                viewBox="0 0 24 24"
+                width="18px"
+                fill="#FFFFFF"
+              >
+                <path d="M0 0h24v24H0V0z" fill="none" />
+                <path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z" />
+              </svg>
             </S.Copy>
           </S.ColorCode>
         </S.Color>
@@ -69,7 +78,16 @@ export const Palette = ({ palette }) => {
                 navigator.clipboard.writeText(displayPaletteColors(2));
               }}
             >
-              <CopyIcon />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="18px"
+                viewBox="0 0 24 24"
+                width="18px"
+                fill="#FFFFFF"
+              >
+                <path d="M0 0h24v24H0V0z" fill="none" />
+                <path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z" />
+              </svg>
             </S.Copy>
           </S.ColorCode>
         </S.Color>
@@ -81,7 +99,16 @@ export const Palette = ({ palette }) => {
                 navigator.clipboard.writeText(displayPaletteColors(3));
               }}
             >
-              <CopyIcon />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="18px"
+                viewBox="0 0 24 24"
+                width="18px"
+                fill="#FFFFFF"
+              >
+                <path d="M0 0h24v24H0V0z" fill="none" />
+                <path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z" />
+              </svg>
             </S.Copy>
           </S.ColorCode>
         </S.Color>
@@ -93,7 +120,16 @@ export const Palette = ({ palette }) => {
                 navigator.clipboard.writeText(displayPaletteColors(0));
               }}
             >
-              <CopyIcon />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="18px"
+                viewBox="0 0 24 24"
+                width="18px"
+                fill="#FFFFFF"
+              >
+                <path d="M0 0h24v24H0V0z" fill="none" />
+                <path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z" />
+              </svg>
             </S.Copy>
           </S.ColorCode>
         </S.Color>
