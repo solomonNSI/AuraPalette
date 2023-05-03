91de282abc477e4e2d435c38b657c81c2e090f90
CanAvsar <can.avsar@ug.bilkent.edu.tr>
2023-05-03 13:59:49 +0300

    Other palettes' height

--- aura-palette/src/Components/Palette/style.js
+++ aura-palette/src/Components/Palette/style.js
@@ -140,7 +140,7 @@ export const ColorBlindColors = styled.div`
     background-color: #ffffff;
     border-radius: 8px;
     padding: 20px;
-    height: 140px;
+    height: 145px;
 
     &.dark {
       background-color: #000000;
