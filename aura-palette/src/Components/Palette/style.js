08853e9f180280e24fb4fad325fb8241bb2bcf78
CanAvsar <can.avsar@ug.bilkent.edu.tr>
2023-05-11 16:11:28 +0300

    Choose from right text style UI

--- aura-palette/src/Components/Palette/style.js
+++ aura-palette/src/Components/Palette/style.js
@@ -142,6 +142,22 @@ export const ColorBlindColors = styled.div`
     padding: 20px;
     height: 145px;
 
+    &.choose {
+      border: 1px dashed grey;
+      background-color: #eeeeee;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      .chooseText {
+        color: #555555;
+        font-weight: 400;
+        text-align: center;
+        max-width: 300px;
+        margin: 0;
+      }
+    }
+
     &.dark {
       background-color: #000000;
     }
