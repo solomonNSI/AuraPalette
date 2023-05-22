12831d71498ed643e7032b84d3939807a1a881d4
CanAvsar <can.avsar@ug.bilkent.edu.tr>
2023-05-22 15:35:54 +0300

    " for queries

--- aura-palette/src/Components/MiniPalette/MiniPalette.js
+++ aura-palette/src/Components/MiniPalette/MiniPalette.js
@@ -14,7 +14,7 @@ export const MiniPalette = ({ DarkMode, palette, query }) => {
     <S.Container className = {DarkMode}>
       <S.Header className = {DarkMode}>
         <S.Titles className = {DarkMode}>
-          <S.PaletteTitle className = {DarkMode}>Query: {query}</S.PaletteTitle>
+          <S.PaletteTitle className = {DarkMode}>Query: "{query}"</S.PaletteTitle>
           {/* <S.Date className = {DarkMode}>Enter Creation Date Here</S.Date> */}
         </S.Titles>
 
