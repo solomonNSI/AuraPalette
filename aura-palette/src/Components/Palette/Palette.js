f8440e86a9338451d7eaf459f1dd27d60b72015b
Zeynepnur Cavcar <zeynepnur.cavcar@ug.bilkent.edu.tr>
2023-05-23 12:00:32 +0300

    bug fix CRITICAL

--- aura-palette/src/Components/Palette/Palette.js
+++ aura-palette/src/Components/Palette/Palette.js
@@ -100,8 +100,7 @@ export const Palette = ({ palette, lock, setLock, setHarmony, harmony, setEdited
         var xmlhttp = new XMLHttpRequest();
         var token_to_check;
         var loggedIn = false;
-        //xmlhttp.open("GET", "https://may22-vhxzdlegrq-ew.a.run.app/account/checktoken/");
-        xmlhttp.open("GET", "http://127.0.0.1:8000/account/checktoken/");
+        xmlhttp.open("GET", "https://may22-vhxzdlegrq-ew.a.run.app/account/checktoken/");
         xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
         xmlhttp.setRequestHeader('Authorization', 'Bearer ' + sessionStorage.getItem('user_token'));
         xmlhttp.onload  = function() {
